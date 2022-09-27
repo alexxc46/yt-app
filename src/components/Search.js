cd6d0c68853442cd1d0f3dff9cc436aed5ac359e
Alex Kram <alexxc46@gmail.com>
2022-09-27 10:50:29 +0300

    all-committed

---
+++ src/components/Search.js
@@ -0,0 +1,71 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const Search = () => {
+    const [term, setTerm] = useState("programming");
+    const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term
+                }
+            })
+            setResults(data.query.search);
+        };
+
+        const timeoutId = setTimeout(() => {
+            if(term) {
+                search();
+            }
+        }, 1000);
+
+        return () => {
+            clearTimeout(timeoutId);
+        }
+
+    }, [term])
+
+    const renderedResults = results.map(res => {
+        return (
+            <div key={res.pageid} className={"item"}>
+                <div className={"right floated content"}>
+                    <a
+                        className={"ui button"}
+                        href={`https://en.wikipedia.org?curid=${res.pageid}`}
+                    >Go</a>
+                </div>
+                <div className={"content"}>
+                    <div className={"header"}>
+                        {res.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html: res.snippet }}></span>
+                </div>
+            </div>
+        )
+    })
+    return (
+        <div>
+            <div className={"ui form"}>
+                <div className={"field"}>
+                    <label>Enter somthgint</label>
+                    <input
+                        value={term}
+                        onChange={e => setTerm(e.target.value)}
+                        className={"input"}
+                    />
+                </div>
+            </div>
+            <div className={"ui celled list"}>
+                {renderedResults}
+            </div>
+        </div>
+    )
+}
+
+export default Search;
